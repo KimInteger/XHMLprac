feeb557bad9d30793a1c46cb0b6bd5b07a905b3c
KimInteger <kjs20151240@gmail.com>
2024-05-28 21:06:31 +0900

    xhr.send를 안해서 console에 찍히지 않은것이었다.

--- public/script.js
+++ public/script.js
@@ -14,7 +14,7 @@ const getPokemonAJAX = () => {
   const xhr = new XMLHttpRequest(); // 이것이 '객체', '인스턴스'로 보여야 한다.
   // ! 객체로 조회된다. [object XMLHttpRequest]
   console.dir('데이터 받기전 조회 : ' + xhr);
-  xhr.open("GET", "https://pokeqpi.co/api/v2/pokemon/",true);
+  xhr.open("GET", "https://pokeapi.co/api/v2/pokemon/",true);
   // 여기에 true는 비동기 요청을 의미한다.
   // 비동기 요청을 하면 요청을 보낸 후 응답이 올 때까지 기다리지 않고 다음코드를 실행한다.
   // 따라서 응답이 올 때까지 기다렸다가 다음 코드를 실행하고 싶다면 false로 설정해야 한다.
@@ -26,8 +26,15 @@ const getPokemonAJAX = () => {
       console.dir(xhr); // xhr은 객체이다.
 
       const result = JSON.parse(xhr.responseText);
+      // 파일을 검사해본결과 JSON 형식으로 되어있는 것을 확인할 수 있다.
+      console.dir(result);
+
+      const pokemons = result.results;
+      console.dir(pokemons);
+      // pokemons는 배열인 것을 결과를 통해 확인 할 수 있다.
     }
   });
+  xhr.send();
 };
 
 getPokemonAJAX();
